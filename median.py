8be1255af2c38c984dbca1e75e8aa3f6996ea6e8
Conoisseur <97054370+Conoisseur@users.noreply.github.com>
2022-10-09 20:25:25 +0100

    did it
    lol

--- median.py
+++ median.py
@@ -9,4 +9,18 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+print(f'your list {numbers}')
+length_of_list = len(numbers)
+median = 0
+
+numbers.sort()
+
+if length_of_list % 2 == 0:
+    number1,number2 = numbers[length_of_list//2],numbers[(length_of_list//2)-1]
+    median = (number1+number2) / 2
+else:
+    median = numbers[length_of_list//2]
+
+print(f'median: {median}')
+input('Press ENTER to exit')
